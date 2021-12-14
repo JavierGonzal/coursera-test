6a5b315f62fab06a5fc8b5d5f816f4a16d15c399
Javier Gonzalez <javier.gonzalez@nexplore.com>
2021-12-14 15:45:47 +0100

    added new

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -80,22 +80,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 // *** start ***
 // On first load, show home view
-showLoading("#main-content");
-$ajaxUtils.sendGetRequest(
-  homeHtml,
-  function (responseText) {
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
-  },
-  false);
-});
-/*dc.loadSpecialCategories = function () {
+dc.loadSpecialCategories = function () {
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
     allCategoriesUrl,
     buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
     true); // Explicitly setting the flag to get JSON from server processed into an object literal
-};*/
+};
 // *** finish **
 
 
